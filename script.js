05cd0f50d4a4375caa1a6ee172524e9129785630
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2020-01-28 10:30:30 +0100

    Random Color generator button final version

--- script.js
+++ script.js
@@ -41,6 +41,19 @@ function displayRandomBackground() {
 }
 
 
+    // this part displays current colors on-load
+function displayInitialGradient() {
+
+    body.style.background =
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
 
 
 color1.addEventListener("input", setGradient);
